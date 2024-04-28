703a19db2ccd9fea93d2419488f570ded7e08618
yoaruku <rhrudgh12@naver.com>
2024-04-29 00:54:27 +0900

    #12 #17 #21 #28 feat:Create MainMenuView

---
+++ src/main/java/com/ohracleLand/model/dto/NoticeDTO.java
@@ -0,0 +1,68 @@
+package com.ohracleLand.model.dto;
+
+public class NoticeDTO {
+    private int no;
+    private String title;
+    private String content;
+    private int views;
+    private String user_name;
+
+    private static int cnt = 0;
+
+    public NoticeDTO(String title, String content, int views, String user_name) {
+        this.no = cnt + 1;
+        this.cnt = this.no;
+        this.title = title;
+        this.content = content;
+        this.views = views;
+        this.user_name = user_name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    @Override
+    public String toString() {
+        return "NoticeDTO{" +
+                "no=" + no +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", views=" + views +
+                ", user_name='" + user_name + '\'' +
+                ", cnt=" + cnt +
+                '}';
+    }
+}
