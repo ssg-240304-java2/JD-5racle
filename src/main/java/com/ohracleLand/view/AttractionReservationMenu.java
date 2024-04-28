35d17731e1ace558329bfd46ec1e6b48aedf6179
Lucas-Choi-17 <csissis@naver.com>
2024-04-28 19:51:30 +0900

    fixed: merged attraction menu selection into AttractionReservationMenu.java

--- src/main/java/com/ohracleLand/view/AttractionReservationMenu.java
+++ src/main/java/com/ohracleLand/view/AttractionReservationMenu.java
@@ -1,17 +1,37 @@
 package com.ohracleLand.view;
 
+import com.ohracleLand.model.dto.UserDTO;
+
 import java.util.Scanner;
 
 public class AttractionReservationMenu {
-    public static int menu() {
-        System.out.print("""
-                ===============================================
-                                      메뉴
-                ===============================================
-                1. 놀이기구 예약
-                2. 예약 목록 확인
-                -----------------------------------------------
-                원하는 메뉴를 입력하세요 :\s""");
-        return new Scanner(System.in).nextInt();
+
+    public static void menu(UserDTO user) {
+        AttractionReservation attractionReservation = new AttractionReservation(user);
+        while (true) {
+            System.out.print("""
+                    ===============================================
+                                          메뉴
+                    ===============================================
+                    1. 놀이기구 예약
+                    2. 예약 목록 확인
+                    -----------------------------------------------
+                    원하는 메뉴를 입력하세요 :\s""");
+            int input = new Scanner(System.in).nextInt();
+
+
+            switch (input) {
+                case 1:
+                    attractionReservation.selectAttraction();
+                    break;
+                case 2:
+                    attractionReservation.reservationManagement();
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("값을 정확히 입력해주세요");
+            }
+        }
     }
 }
