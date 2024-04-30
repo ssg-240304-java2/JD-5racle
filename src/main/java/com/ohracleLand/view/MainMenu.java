40ddb23902bcdfb1c6e258d74f763d2b3736897f
gusrbstjr <gusrbstjr@naver.com>
2024-04-30 18:03:52 +0900

    REAMDE 작성

--- src/main/java/com/ohracleLand/view/MainMenu.java
+++ src/main/java/com/ohracleLand/view/MainMenu.java
@@ -1,5 +1,6 @@
 package com.ohracleLand.view;
 
+import com.ohracleLand.controller.BoardManager;
 import com.ohracleLand.controller.LostAndFoundManager;
 import com.ohracleLand.controller.NoticeManager;
 import com.ohracleLand.model.dto.UserDTO;
@@ -12,11 +13,13 @@ public class MainMenu {
     NoticeManager nm = new NoticeManager();
     LostAndFoundManager lostM = new LostAndFoundManager();
 
+    BoardManager bm = new BoardManager();
+
     //(+) 메인메뉴 테스트코드 삭제예정
     public static void main(String[] args) {
         // test mainMenu
         MainMenu m = new MainMenu();
-//        m.mainMenu();
+        m.mainMenu(null);
     }
     //(-) 메인메뉴 테스트코드 삭제예정
 
@@ -51,6 +54,7 @@ public class MainMenu {
                     break;
                 case 4: // 고객의 소리
                     // 메소드 호출
+                    bm.view_Board();
                     break;
                 case 5: // 공지사항
                     // 메소드 호출
